ef0f9e15458b0ff3a0089578c4d8ec6bed65d68b
Merkee <mercensutoku@gmail.com>
2021-01-15 15:26:25 +0300

    Working with Contacts

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,23 +1,45 @@
 import style from './Contacts.module.css';
+import ph from './Phones.png';
 
 let Contacts = () => {
     return(
         <div className={style.Contacts}>
             <div className={style.PhonesBox}>
                 <div className={style.Title}>
-                    <img src="https://s1.iconbird.com/ico/2013/8/429/w512h5121377939352185074notebookstreamline.png"/>
-                    <h2>Телефоны</h2>
+                    <h2>Отдел продаж</h2>
                 </div>
                 <div className={style.Phones}>
                     <div className={style.PhonesGrid}>
                         <ul>
                             <li>
-                                
+                                <div className={style.TitlePhones}>Отдел сбыта</div>
+                                <a className={style.ItemPhones} href="tel:+375772281337">+375772281337</a>
+                            </li>
+                            <li>
+                                <div className={style.TitlePhones}>Отдел маркетинга</div>
+                                <a className={style.ItemPhones} href="tel:+375772281337">+375772281337</a>
+                            </li>
+                            <li>
+                                <div className={style.TitlePhones}>Начальник отдела продаж</div>
+                                <div className={style.FIO}>Стома Марина Вячеславовна</div>
+                                <a className={style.ItemPhones} href="tel:+375772281337">+375772281337</a>
+                            </li>
+                            <li>
+                                <div className={style.TitlePhones}>Национальный менеджер по продажам</div>
+                                <div className={style.FIO}>Феминист Анна Георгиевна</div>
+                                <a className={style.ItemPhones} href="tel:+375772281337">+375772281337</a>
+                            </li>
+                            <li>
+                                <div className={style.TitlePhones}>Первый зам.директора (по экономике и маркетингу)</div>
+                                <div className={style.FIO}>Сачевичик Анна Мечеславовна</div>
+                                <a className={style.ItemPhones} href="tel:+375772281337">+375772281337</a>
+                            </li>
+                            <li>
+                                <div className={style.TitlePhones}>Начальник отдела внешнеэкономической деятельности</div>
+                                <div className={style.FIO}>Василевич Алла Григорьевна</div>
+                                <a className={style.ItemPhones} href="tel:+375772281337">+375772281337</a>
                             </li>
                         </ul>                    
-                    </div>
-                    <div className={style.PhonesGrid}>
-                        s                    
                     </div>
                 </div>
             </div>
